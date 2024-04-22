649a7c7c3ba85a0c99b05238f02ad4c5c75bf46f
TheKeep <167775939+TheKeepCode@users.noreply.github.com>
2024-04-22 09:10:47 -0400

    Add files via upload
    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# redalert
-Tool
+# Red Alert
+
+This Python3 script should be run on your Windows computer in a separate offscreen terminal while playing the game.  It will alert you if there is a "Bad" or "Terrible" player on your screen both audibly and via text output via the terminal.
+
+
+## Screenshot Parameters and Tips
+
+This script works by frequently taking a screenshot of an area of your monitor that you define (via screenx and screeny parameters) and it compares it against one or more image patterns that you define (via alertimages parameter).  This script comes with two files, one called "terrible.png" and another called "neutral.png" to help you get started.  If this script sees a match of what's on your screen against the images you tell it to look for, it will make a sound and output some text in your PowerShell window.
+
+The screenx parameter is the X-coordinate of your screenshot, where the first number is the starting position and the second number is your ending position.  So, if you define `--screenx 100,500`, it will take screenshots from the 100th pixel from the left, all the way to the 500th pixed from the left, for a total of 400 pixels wide.
+
+The screeny parameter is the Y-coordinate of your screenshot, where the first number is the starting position and the second number is your ending position.  So, if you define `--screeny 200,800`, it will take screenshots from the 200th pixel from the top, all the way to the 800th pixed from the top, for a total of 600 pixels high.
+
+Combining the screenx and screeny parameters gives you a square - a rectangular area of your screen that this tool will take a screenshot of.  The smaller this screenshot, the faster the Python script will run and the less resource-intensive the tool will be when it's running.
+
+You should set your screen parameters (--screenx and --screeny) to your Local window.  You can use the "screenshot_logs" directory and the "latest_screenshot.png" file to determine the current view that the script sees when it's running.  Once you have the tool working for you and you verified the screenshots are taking the section of the screen that you expect it to, you can improve the tool's performance by passing the `--nodisk` option which will stop the script from saving screenshots to your disk.
+
+
+## Set Up - Run These Once
+
+1. Install the latest version of Python3 from Microsoft Store: https://apps.microsoft.com/search/publisher?name=Python+Software+Foundation&hl=en-us&gl=US
+ * This script has been tested with Python3.12
+2. Open PowerShell
+3. Copy and paste the following, then press Enter: `pip3 install pyautogui`
+4. Copy and paste the following, then press Enter: `pip3 install pygame`
+5. Copy and paste the following, then press Enter: `pip3 install numpy`
+6. Copy and paste the following, then press Enter: `pip3 install Pillow`
+
+
+## Normal Usage
+
+1. Open PowerShell
+2. Change directory to where this code is on your computer by using the `cd` command, entering in the directory, and then pressing Enter
+3. Copy and paste the following, then press Enter: `python3 main.py --screenx 0,1920 --screeny 0,1440 --alertimages terrible.png`
+4. When you want to quit, click on your PowerShell session, hold CTRL key, and press the C key
+
+
+## Known Limitations and other FAQ
+
+* This script can only read from the primary monitor
+* The function `pyautogui.locate` does not see colors, so "Bad" and "Terrible" will both alarm
+* The images that are included with this script called "terrible.png" and "neutral.png" may not work if you have changed how these appear in your game client - overwrite or include new images as you deem appropriate
---
+++ main.py
@@ -0,0 +1,145 @@
+import pyautogui
+import pygame
+import time
+import argparse
+import datetime
+import numpy as np
+from PIL import Image, ImageDraw
+import os
+
+# Initialize pygame for playing sounds
+pygame.init()
+
+# Global variable to keep track of modified screenshots
+modified_screenshots = []
+screenshot_counter = 0
+detected_counter = 0
+
+def is_pattern_present(pattern_image, screenshot):
+    try:
+        location = pyautogui.locate(pattern_image, screenshot)
+        return location is not None
+    except pyautogui.ImageNotFoundException:
+        return False
+
+def load_sounds(alert_images):
+    sounds = {}
+    for alert_image in alert_images:
+        sounds[alert_image] = "beep.wav"  # Use beep.wav for all alert images
+    return sounds
+
+def save_screenshot_with_timestamp(screenshot, alert_image, screenshot_logs_dir, nodisk):
+    if nodisk == False:
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")[:-1]
+        filename = os.path.join(screenshot_logs_dir, f"screenshot_{timestamp}_{alert_image}")
+        screenshot.save(filename)
+        if verbose:
+            print(f"Modified screenshot saved as {filename}")
+        # Add the filename to the list of modified screenshots
+        modified_screenshots.append(filename)
+        # Check if there are more than 10 modified screenshots, and delete the oldest one if necessary
+        if len(modified_screenshots) > 10:
+            os.remove(modified_screenshots.pop(0))  # Remove the oldest screenshot file
+
+def highlight_pattern(image, location):
+    overlay = np.array(image)
+    border_width = 12  # Set border width to 12
+    expanded_location = (
+        (location.left - border_width, location.top - border_width),
+        (location.left + location.width + border_width, location.top + location.height + border_width)
+    )
+    draw = ImageDraw.Draw(image)
+    draw.rectangle(expanded_location, outline=(255, 255, 0), width=2)  # Bright yellow rectangle
+    return image  # Return the modified image
+
+def save_latest_screenshot(screenshot, screenshot_logs_dir, nodisk):
+    if nodisk == False:
+        filename = os.path.join(screenshot_logs_dir, "latest_screenshot.png")
+        screenshot.save(filename)
+        if verbose:
+            print("Latest screenshot saved as latest_screenshot.png")
+
+def main(screen_x_range, screen_y_range, alert_images, verbose, nodisk, frequency):
+    global detected_counter, screenshot_counter  # Declare global variables
+
+    # Initialize screenshot_counter
+    screenshot_counter = 0
+
+    start_x, end_x = map(int, screen_x_range.split(','))
+    start_y, end_y = map(int, screen_y_range.split(','))
+    screen_width, screen_height = pyautogui.size()
+    start_x = max(min(start_x, screen_width - 1), 0)
+    end_x = max(min(end_x, screen_width), 1)
+    start_y = max(min(start_y, screen_height - 1), 0)
+    end_y = max(min(end_y, screen_height), 1)
+
+    sounds = load_sounds(alert_images)
+
+    # Create screenshot_logs directory if it doesn't exist
+    screenshot_logs_dir = "screenshot_logs"
+    if not os.path.exists(screenshot_logs_dir):
+        os.makedirs(screenshot_logs_dir)
+    else:
+        # Clear the contents of screenshot_logs directory
+        for filename in os.listdir(screenshot_logs_dir):
+            file_path = os.path.join(screenshot_logs_dir, filename)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(f"Error deleting {file_path}: {e}")
+
+    try:
+        while True:
+            detected = False
+            screenshot = pyautogui.screenshot(region=(start_x, start_y, end_x - start_x, end_y - start_y))
+
+            # Check for consecutive detections
+            for alert_image in alert_images:
+                if is_pattern_present(alert_image, screenshot):
+                    detected = True
+                    location = pyautogui.locate(alert_image, screenshot, confidence=0.99)
+                    screenshot_modified = highlight_pattern(screenshot.copy(), location)
+                    save_screenshot_with_timestamp(screenshot_modified, alert_image, screenshot_logs_dir, nodisk)
+                
+                    if detected_counter > 1:
+                        sound_file = sounds[alert_image]
+                        pygame.mixer.Sound(sound_file).play()
+                        print(f"Pattern {alert_image} detected! Sound played.")
+                        time.sleep(1.5)
+                        break
+                    else:
+                        print(f"Pattern {alert_image} detected, below detected_counter threshold, so no sound played.")
+
+            if detected:
+                detected_counter += 1
+                if verbose:
+                    print(f"Detected Counter at {detected_counter} since pattern was detected.")
+                if detected_counter > 20:
+                    detected_counter = 5
+            else:
+                detected_counter = 0
+                if verbose:
+                    print(f"Detected Counter at {detected_counter} since pattern was not detected.")
+
+            save_latest_screenshot(screenshot, screenshot_logs_dir, nodisk)
+            time.sleep(frequency)
+
+    except KeyboardInterrupt:
+        print("\nScript stopped.")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Search for patterns on the screen and play sounds when detected.")
+    parser.add_argument("--screenx", type=str, default="0,1920", help="Screen region for x dimension (start,end)")
+    parser.add_argument("--screeny", type=str, default="0,1080", help="Screen region for y dimension (start,end)")
+    parser.add_argument("--alertimages", default="terrible.png", nargs="+", help="Filenames of alert images")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose mode to print additional messages")
+    parser.add_argument("--nodisk", action="store_true", help="Disables storing screenshots to the disk")
+    parser.add_argument("--frequency", type=float, default=0.5, help="Frequency of taking screenshots in seconds")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    verbose = args.verbose  # Store the value of verbose for later use
+    nodisk = args.nodisk  # Store the value of nodisk for later use
+    main(args.screenx, args.screeny, args.alertimages, args.verbose, args.nodisk, args.frequency)
