06ae545f5388ff544581f45f5ad4eaf014c116db
TheKeep <no@reply.com>
2024-05-01 15:08:34 -0400

    Working on primary, testing on secondary monitor

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import time
 import argparse
 import datetime
 import numpy as np
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageGrab
 import os
+import mss
 
 # Get the directory path of the script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -29,11 +30,7 @@ def load_sounds(alert_images):
     sounds = {}
     script_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory path of the script
     for alert_image in alert_images:
-        filename = os.path.basename(alert_image)  # Extract just the filename from the provided path
-        print(f"filename {filename}")
-        image_path = os.path.join(script_dir, filename)  # Construct the full path relative to the script's directory
-        print(f"image_path {image_path}")
-        sounds[alert_image] = image_path
+        sounds[alert_image] = os.path.join(script_dir, "beep.wav")  # Hardcoded sound file name
     return sounds
 
 def save_screenshot_with_timestamp(screenshot, alert_image, screenshot_logs_dir, nodisk):
@@ -41,8 +38,6 @@ def save_screenshot_with_timestamp(screenshot, alert_image, screenshot_logs_dir,
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")[:-1]
         filename = os.path.join(script_dir, screenshot_logs_dir, f"screenshot_{timestamp}_{alert_image}")
         screenshot.save(filename)
-        if verbose:
-            print(f"Modified screenshot saved as {filename}")
         # Add the filename to the list of modified screenshots
         modified_screenshots.append(filename)
         # Check if there are more than 10 modified screenshots, and delete the oldest one if necessary
@@ -64,8 +59,6 @@ def save_latest_screenshot(screenshot, screenshot_logs_dir, nodisk):
     if not nodisk:
         filename = os.path.join(script_dir, screenshot_logs_dir, "latest_screenshot.png")
         screenshot.save(filename)
-        if verbose:
-            print("Latest screenshot saved as latest_screenshot.png")
 
 def main(screen_x_range, screen_y_range, alert_images, verbose, nodisk, frequency):
     global detected_counter, screenshot_counter  # Declare global variables
@@ -100,7 +93,9 @@ def main(screen_x_range, screen_y_range, alert_images, verbose, nodisk, frequenc
     try:
         while True:
             detected = False
-            screenshot = pyautogui.screenshot(region=(start_x, start_y, end_x - start_x, end_y - start_y))
+            
+            # Capture screenshot
+            screenshot = ImageGrab.grab(bbox=(start_x, start_y, end_x, end_y))
 
             # Check for consecutive detections
             for alert_image in alert_images:
